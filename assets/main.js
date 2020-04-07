366136f4d4c19d93657bbab56d32c76a480ea452
Voggastur <v3x0r_x014@hotmail.com>
2020-04-07 18:37:04 +0000

    added player, enemies, missiles, collisiondetector

--- assets/main.js
+++ assets/main.js
@@ -6,6 +6,24 @@ var hero = {
 
 var missiles = [];
 
+var enemies = [
+    { left: 200, top: 100 },
+    { left: 300, top: 100 },
+    { left: 400, top: 100 },
+    { left: 500, top: 100 },
+    { left: 600, top: 100 },
+    { left: 700, top: 100 },
+    { left: 800, top: 100 },
+    { left: 900, top: 100 },
+    { left: 200, top: 160 },
+    { left: 300, top: 160 },
+    { left: 400, top: 160 },
+    { left: 500, top: 160 },
+    { left: 600, top: 160 },
+    { left: 700, top: 160 },
+    { left: 800, top: 160 },
+];
+
 document.onkeydown = function (e) {
     console.log(e.keyCode);
 
@@ -45,9 +63,44 @@ function moveMissiles() {
     }
 }
 
+function drawEnemies() {
+    document.getElementById('enemies').innerHTML = "";
+    for (var enemy = 0; enemy < enemies.length; enemy = enemy + 1) {
+        document.getElementById('enemies').innerHTML +=
+            `<div class='enemy' style='left:${enemies[enemy].left}px; 
+                top:${enemies[enemy].top}px;'></div>`;
+    }
+}
+
+function moveEnemies() {
+    for (var enemy = 0; enemy < enemies.length; enemy = enemy + 1) {
+        enemies[enemy].top = enemies[enemy].top + 2;
+    }
+}
+
+function collisionDetection() {
+    for (var enemy = 0; enemy < enemies.length; enemy = enemy + 1) {
+        for (var missile = 0; missile < missiles.length; missile = missile + 1) {
+            if (
+                (missiles[missile].top <= enemies[enemy].top + 50) &&
+                (missiles[missile].top >= enemies[enemy].top) &&
+                (missiles[missile].left >= enemies[enemy].left) &&
+                (missiles[missile].left <= enemies[enemy].left + 50)
+            ) {
+                console.log("HIT");
+                enemies.splice(enemy, 1);
+                missiles.splice(missile, 1);
+            }
+        }
+    }
+}
+
 function gameLoop() {
     setTimeout(gameLoop, 100)
     moveMissiles();
     drawMissiles();
+    moveEnemies();
+    drawEnemies();
+    collisionDetection();
 }
 gameLoop();
