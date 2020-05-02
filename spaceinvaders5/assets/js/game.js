405b1130f0d8c9ae6ac55ec21a73bfe70dd2f501
Voggastur <v3x0r_x014@hotmail.com>
2020-05-02 21:58:45 +0000

    functional energybomb, functional collisionDetection added

--- spaceinvaders5/assets/js/game.js
+++ spaceinvaders5/assets/js/game.js
@@ -2,8 +2,11 @@ $(document).ready(function () {
     $(".button").click(function () {
         $(".buttoncontainer").slideUp("slow");
         $("#background").show();
-        $("#score").html("Score " + score);
-        setInterval(triggerBombs, 1500)
+        var score = sessionStorage.getItem("score");
+        $(".score").html("Score " + score);
+        setInterval(removeBombs, 4750);
+        removeBombs();
+        setInterval(triggerBombs, 4750);
         triggerBombs();
         setInterval(gameLoop, 50);
         gameLoop();
@@ -13,6 +16,7 @@ $(document).ready(function () {
 // window.location.assign(http://...game.html?score=3) ----- one way to remember score between rounds //
 
 var score = 0;
+var scoreUpdate = 0;
 
 var lasers = [];
 var maxLasers = 5;
@@ -31,11 +35,7 @@ var aliens = [
     { left: 1150, top: 450, height: 30, width: 30 },
     { left: 1150, top: 500, height: 30, width: 30 },
     { left: 1150, top: 550, height: 30, width: 30 },
-    { left: 1150, top: 600, height: 30, width: 30 },
-    { left: 1050, top: 350, height: 30, width: 30 },
-    { left: 1050, top: 400, height: 30, width: 30 },
-    { left: 1050, top: 450, height: 30, width: 30 },
-    { left: 1050, top: 500, height: 30, width: 30 }
+    { left: 1150, top: 600, height: 30, width: 30 }
 ];
 
 var alienStep = - 50; // As aliens touch the boundaries on the transversal path, they will take a step -50 closer to the player
@@ -43,7 +43,6 @@ var alienDirection = 3; // Direction of aliens will switch from either +3 or -3 
 
 const height = $("#background").height();
 const width = $("#background").width();
-console.log(height, width);
 
 var spaceship = {
     top: 400,
@@ -98,7 +97,7 @@ function moveSpaceship() { // moveSpaceship() function is called every time we d
 }
 
 function pushRockets() {
-    document.getElementById("rockets").innerHTML = ""; // An empty string is set to remove the image from the last loop, before the new paste is made
+    $("#rockets").html(""); // An empty string is set to remove the image from the last loop, before the new paste is made
     for (var rocket = 0; rocket < rockets.length; rocket += 1) {
         document.getElementById("rockets").innerHTML += `<div class='rocket' style='left:${rockets[rocket].left}px; top:${rockets[rocket].top}px;'></div>`;
         // rockets are pasted onto HTML using handy template literals
@@ -113,7 +112,7 @@ function moveRockets() {
 };
 
 function pushLasers() {
-    document.getElementById("lasers").innerHTML = ""; // An empty string is set to remove the image from the last loop, before the new paste
+    $("#lasers").html(""); // This removes previous image from the previous iteration before the new paste in the next for loop, creating illusion of moving
     for (var laser = 0; laser < lasers.length; laser += 1) {
         document.getElementById("lasers").innerHTML += `<div class='laser' style='left:${lasers[laser].left}px; top:${lasers[laser].top}px;'></div>`;
         // lasers are pasted the same way as rockets
@@ -123,11 +122,14 @@ function pushLasers() {
 function moveLasers() {
     for (var laser = 0; laser < lasers.length; laser += 1) {
         lasers[laser].left += 18; // laser speed is set to move faster than the rockets
+        if (lasers.left > width || lasers.left < width) {
+            lasers.splice(laser, 1);
+        }
     };
 };
 
 function triggerBombs() {
-    for (var energy = 0; energybomb < energy.length; energybomb += 1) {
+    for (var alien = 0; alien < aliens.length; alien += 1) {
         energy.push({
             top: aliens[0].top,
             left: aliens[0].left
@@ -136,12 +138,20 @@ function triggerBombs() {
     };
 };
 
-function pushBombs() { // this function handles the enemy weapons
-    document.getElementById("energy").innerHTML = "";
+function removeBombs() {
     for (var alien = 0; alien < aliens.length; alien += 1) {
+        for (var i = 0; i < energy.length; i += 1) {
+            energy.splice(i, 1);
+        };
+    };
+};
+
+function pushBombs() { // this function handles the enemy weapons
+    $("#energy").html(""); // this removes previous image from the previous iteration before the new paste in the next for loop
+    for (var energybomb = 0; energybomb < energy.length; energybomb += 1) {
         document.getElementById("energy").innerHTML += `<div class='energybomb' style='left:${energy[energybomb].left}px;
         top:${energy[energybomb].top}px;'></div>`;
-        // energyBombs are placed similarly as other weapons, but use another trigger on a setInterval at the top
+        // energyBombs are placed similarly as other weapons, but use another trigger on a setInterval(1500) at the top
     };
 };
 
@@ -151,9 +161,9 @@ function moveBombs() {
     };
 };
 
-function drawAliens() {
-    document.getElementById("aliens").innerHTML = ""; // An empty string is set to remove the image from the last loop, before the new paste
-    for (var alien = 0; alien < aliens.length; alien += 1) {
+function pushAliens() {
+    $("#aliens").html(""); // An empty string is set to remove the image from the last loop, before the new paste
+    for (var alien = 0; alien < aliens.length; ++alien) {
         // This for loop will repeat for as many aliens there are, in every interval
         document.getElementById("aliens").innerHTML += `<div class='alien' style='left:${aliens[alien].left}px; 
         top:${aliens[alien].top}px;'></div>`;
@@ -162,7 +172,7 @@ function drawAliens() {
 };
 
 function moveAliens() {
-    for (var alien = 0; alien < aliens.length; alien += 1) {
+    for (var alien = 0; alien < aliens.length; ++alien) {
         aliens[alien].top += alienDirection;
         if (aliens[alien].top > 775 || aliens[alien].top < 25) {
             switch (alienDirection) {
@@ -178,53 +188,61 @@ function moveAliens() {
         };
     };
     function alienAxis() {
-        for (var alien = 0; alien < aliens.length; alien += 1)
+        for (var alien = 0; alien < aliens.length; ++alien)
             aliens[alien].left += alienStep;
     };
 };
 
-function distBetweenPoints(x1, y1, x2, y2) { // Used in collisionDetection, function with advanced Maths
-    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-};
-
 function collisionDetection() {
-    for (var alien = 0; alien < aliens.length; alien += 1) {
-        for (var rocket = 0; rocket < rockets.length && rocket > maxRockets; rocket += 1) {
-            if (rockets[rocket].top <= aliens[alien].top - 70 &&
-                rockets[rocket].top >= aliens[alien].top - 40 &&
+    for (var alien = 0; alien < aliens.length; ++alien) {
+        var score = Number;
+        for (var rocket = 0; rocket < rockets.length; ++rocket) {
+            if (rockets[rocket].top <= aliens[alien].top - 30 &&
+                rockets[rocket].top >= aliens[alien].top - 60 &&
                 rockets[rocket].left >= aliens[alien].left &&
                 rockets[rocket].left <= aliens[alien].left + 30) {
                 // Collision has occured, remove weapon and alien from game, increment score
-                aliens.splice(index, 2);
-                rockets.splice(index, 1);
+                score += 1;
+                console.log(score);
+                aliens.splice(alien, 2);
+                rockets.splice(rocket, 1);
                 console.log("ROCKETHIT");
-                gameOver();
+                sessionStorage.setItem("score", "Number(score)"); // Save score to sessionStorage
+                checkGameover();
+                break;
             };
         };
-        for (var laser = 0; laser < lasers.length && laser > maxLasers; laser += 1) {
-            if (lasers[laser].top <= aliens[alien].top -70 &&
-                lasers[laser].top >= aliens[alien].top -40 &&
+        for (var laser = 0; laser < lasers.length; ++laser) {
+            if (lasers[laser].top <= aliens[alien].top - 30 &&
+                lasers[laser].top >= aliens[alien].top - 60 &&
                 lasers[laser].left >= aliens[alien].left &&
                 lasers[laser].left <= aliens[alien].left + 30) {
                 // Collision occured, remove weapon and alien from game, increment score
-                aliens.splice(index, 1);
-                lasers.splice(index, 1);
+                score += 1;
+                console.log(score);
+                aliens.splice(alien, 1);
+                lasers.splice(laser, 1);
                 console.log("LASERHIT");
-                gameOver();
+                sessionStorage.setItem("score", "Number(score)"); // Save score to sessionStorage
+                checkGameover();
+                break;
             };
         };
     };
 };
 
-function gameOver() {
-    if (aliens.length == 0 ||
-        spaceship.top <= energy[energybomb].top + 30 &&
-        spaceship.top >= energy[energybomb].top &&
-        spaceship.left >= energy[energybomb].left &&
-        spaceship.left <= energy[energybomb].left + 30) {
-        $(".buttoncontainer").slideDown("slow");
-        $("#score").css("z-index", "5");
-        $("#score").css("display", "block");
+function checkGameover() {
+    var energybomb = $(".energybomb");
+    if (aliens.length == 0 || // This primary condition is usually why the game ends, no aliens left
+        aliens.left >= width ||
+        energybomb.top <= spaceship.top + 30 && // Trying to add collisionDetection for my own spaceship but its not working
+        energybomb.top >= spaceship.top &&
+        energybomb.left >= spaceship.left &&
+        energybomb.left <= spaceship.left + 30) {
+        $(".buttoncontainer").slideDown("slow"); // slideDown window upon mission complete
+        let score = sessionStorage.getItem("score"); // Save score in sessionStorage
+        $(".score").html("Score " + score); // Show score in class .score h2 element
+        location.reload(); // reload page to repopulate with aliens
     };
 };
 
@@ -236,7 +254,6 @@ function gameLoop() {
     moveBombs();
     pushBombs();
     moveAliens();
-    drawAliens();
+    pushAliens();
     collisionDetection();
 };
-
